a0108e37d560b87a3ea0ed71cd0164b07e1d77fc
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2024-12-22 10:43:03 +0300

    fix(styles): set correct `100vw` width to prevent horizontal scroll when vertical page scrollbar is displayed

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ import VideoPlayerCollection from './VideoPlayer.js'
 import ExpandableContentCollection from './ExpandableContent.js'
 import InputMaskCollection from './InputMask.js'
 import SelectCollection from './Select.js'
+import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js'
 
 new Header()
 new TabsCollection()
@@ -11,3 +12,5 @@ new VideoPlayerCollection()
 new ExpandableContentCollection()
 new InputMaskCollection()
 new SelectCollection()
+
+defineScrollBarWidthCSSVar()
